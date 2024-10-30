91e7334640d2687d619048ede2fd782e94276d7f
João Pedro <leon.jpbs@gmail.com>
2024-10-30 14:21:08 -0300

    Feito o front-end da index

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -12,21 +12,39 @@
 
 @endif
 
-@forelse ($users as $user)
-    ID: {{ $user->id }} <br>
-    Nome: {{ $user->name }} <br>
-    E-mail: {{ $user->email }} <br>
-    <a href="{{ route('user.show', ['user' => $user->id]) }}">Visualizar</a><br>
-    <a href="{{ route('user.edit', ['user' => $user->id]) }}">Editar</a><br>
-    <form method="POST" action="{{ route('user.destroy', ['user' => $user->id]) }}">
-        @csrf
-        @method('delete')
-        <button type="submit" onclick="return confirm('Tem certeza que deseja apagar?')"> Excluir</button>
-    </form>
-    <hr>
-@empty
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">NOME</th>
+            <th scope="col">E-mail</th>
+            <th scope="col" class="text-center">Ações</th>
+        </tr>
+    </thead>
+    <tbody>
 
+    @forelse ($users as $user)
+    <tr>
+        <th scope="row">{{ $user->id }}</th>
+        <td>{{ $user->name }}</td>
+        <td>{{ $user->email }}</td>
+        <td class="text-center">
+                <a class="btn btn-primary btn-sm me-2" href="{{ route('user.show', ['user' => $user->id]) }}">Visualizar</a>
+                <a class="btn btn-warning btn-sm me-2" href="{{ route('user.edit', ['user' => $user->id]) }}">Editar</a>
+                <form method="POST" action="{{ route('user.destroy', ['user' => $user->id]) }}" class="d-inline">
+                    @csrf
+                    @method('delete')
+                    <button class="btn btn-danger btn-sm me-2" type="submit" onclick="return confirm('Tem certeza que deseja apagar?')">Excluir</button>
+                </form>
+        </td>
+    </tr>
+@empty
+    <tr>
+        <td colspan="4" class="text-center">Nenhum usuário encontrado.</td>
+    </tr>
 @endforelse
 
 
-@endsection
+
+
+        @endsection
